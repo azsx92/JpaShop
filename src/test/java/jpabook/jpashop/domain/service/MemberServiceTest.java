34b302586bfb272ba1a147fc0fd17c90ce7e5737
azsx92 <azsx92@gmail.com>
2024-07-31 23:19:12 +0900

    feature 회원가입테스트

---
+++ src/test/java/jpabook/jpashop/domain/service/MemberServiceTest.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.domain.service;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.repository.MemberRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.Assert.*;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Transactional
+public class MemberServiceTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+    @Autowired EntityManager em;
+    @Test
+    public void 회원가입() throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("kim");
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        assertEquals(member, memberRepository.findOne(saveId));
+    }
+
+    @Test
+    public void 중복_회원_예외() throws Exception {
+        //given
+
+        //when
+        //then
+    }
+}
